2dafe2f572073ced6da147800656cdde92e7c20f
Trenton Mosher <starkbewegung@hotmail.com>
2025-03-22 15:42:12 -0700

    splash icon is in, config updated to match

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,11 @@ export {
 
 SplashScreen.preventAutoHideAsync();
 
+SplashScreen.setOptions({
+    duration: 1000,
+    fade: true
+})
+
 // TODO: For global appearance update in the future, read this:
 // https://github.com/facebook/react-native/blob/main/packages/rn-tester/js/examples/Appearance/AppearanceExample.js
 
